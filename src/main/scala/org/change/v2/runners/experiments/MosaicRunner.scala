00657c01a755b6e581ed980efb36f6eee6eaa4e8
crashRT <crashrt1121@gmail.com>
2026-05-27 13:58:47 +0900

    mosaic runnerをファイル出力するように

--- src/main/scala/org/change/v2/runners/experiments/MosaicRunner.scala
+++ src/main/scala/org/change/v2/runners/experiments/MosaicRunner.scala
@@ -1,21 +1,21 @@
 package org.change.v2.runners.experiments
 
-import java.io.{File, FilenameFilter}
+import java.io.{File, FileOutputStream, FilenameFilter, PrintWriter}
 import org.change.parser.clickfile.ClickToAbstractNetwork
 import org.change.parser.interclicklinks.InterClickLinksParser
 import org.change.v2.executor.clickabstractnetwork.ClickExecutionContext
-import org.change.v2.executor.clickabstractnetwork.executionlogging.JsonLogger
-import org.change.v2.analysis.memory.State
+import spray.json._
 
 object MosaicRunner {
   def main(args: Array[String]): Unit = {
     if (args.length < 2) {
-      println("Usage: sbt \"runMain org.change.v2.runners.experiments.MosaicRunner <folder_path> <start_port>\"")
+      println("Usage: sbt \"runMain org.change.v2.runners.experiments.MosaicRunner <folder_path> <start_port> [output_file]\"")
       sys.exit(1)
     }
 
     val folderPath = args(0)
     val startPort = args(1)
+    val outputPath = if (args.length >= 3) args(2) else "out.json"
 
     val clicksFolder = new File(folderPath)
     if (!clicksFolder.exists() || !clicksFolder.isDirectory) {
@@ -45,15 +45,22 @@ object MosaicRunner {
       clicks.map(ClickToAbstractNetwork.buildConfig(_, prefixedElements = true)),
       links,
       startElems = Some(List(startTuple))
-    ).setLogger(JsonLogger)
+    )
 
     val done = ctx.untilDone(verbose = false)
 
-    val out = System.out
-    out.println(
-      done.stuckStates.map(_.jsonString).mkString("Successful: {\n", "\n", "}\n") +
-      done.failedStates.map(_.jsonString).mkString("Failed: {\n", "\n", "}\n")
-    )
-    out.flush()
+    import org.change.v2.analysis.memory.jsonformatters.ExecutionContextToJson._
+
+    val outputFile = new File(outputPath)
+    val outputParent = outputFile.getParentFile
+    if (outputParent != null) {
+      outputParent.mkdirs()
+    }
+
+    val out = new PrintWriter(new FileOutputStream(outputFile))
+    out.println(done.toJson.prettyPrint)
+    out.close()
+
+    println(s"SymNet results written to ${outputFile.getPath}")
   }
 }
